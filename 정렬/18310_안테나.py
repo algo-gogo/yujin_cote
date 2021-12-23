5e8a19e1419839873c6c7f41a8e16ffcd44dcd39
yjcho <yjcho@cavyos.com>
2021-12-23 20:29:37 +0900

    1트 실패. 너무 간단하게 생각햇나봄. total // len(list)가 중간값은 아닌가

---
+++ 정렬/18310_안테나.py
@@ -0,0 +1,17 @@
+'''
+일직선 상의 마을에 여러 채의 집이 위치해있다.
+이중에서 특정 위치의 집에 특별히 한개의 안테나. ㅇㅋㅇㅋ
+모든 집까지의 거리의 총 합이 최소가 되도록. 치킨집문제아녀??
+논리적으로 동일한 위치에 여러개의 집이 존재하는것이 가능. <- 같은위치에값을보관할수있어야됨
+'''
+
+N = int(input())
+house_list = list(map(int, input().split()))
+
+# house_list.sort()
+
+total = 0
+for i in house_list:
+    total += i
+
+print( total // len(house_list) )
